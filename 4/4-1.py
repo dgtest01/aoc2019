6a028f6791d7a383332c302f429e7a6df231c299
Judinous <jorrix@gmail.com>
2019-12-04 09:31:37 -0600

    day 4

---
+++ 4/4-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.6
+
+password = 356261
+passwordMax = 846303
+viablePasswords = 0
+
+while password < passwordMax:
+    characterIndex = 0
+    doubleAdjacentDigits = False
+    while characterIndex < 5:
+        if str(password)[characterIndex] == str(password)[characterIndex + 1]:
+            doubleAdjacentDigits = True
+        characterIndex += 1
+    
+    if doubleAdjacentDigits == True:
+        characterIndex = 0
+        neverDecreases = True
+        while characterIndex < 5:
+            if int(str(password)[characterIndex]) > int(str(password)[characterIndex + 1]):
+                neverDecreases = False
+            characterIndex += 1
+        if neverDecreases == True:
+            viablePasswords += 1
+    password += 1
+print(f"The number of viable passwords is {viablePasswords}")
